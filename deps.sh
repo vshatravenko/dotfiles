56d1c0dd7bff533088d8c7b6a903550647d6c228
Valentine Shatravenko <vshatravenko@heliostech.fr>
2018-09-03 21:02:14 +0300

    Added multiple dependencies to deps.sh

--- deps.sh
+++ deps.sh
@@ -1,13 +1,45 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy --noconfirm ruby unzip
+alias sp="sudo pacman -Sy --noconfirm"
 
+sp go ruby unzip
+
+### Golang
+mkdir -p ${}/{src,pkg,bin}
+
+### Rust
+curl https://sh.rustup.rs -sSf | sh
+
+### Terraform
 curl https://releases.hashicorp.com/terraform/0.11.2/terraform_0.11.2_linux_amd64.zip?_ga=2.132289742.137580065.1516108604-1353905029.1512772481 -o terraform.zip
 
 sudo unzip terraform.zip -d /usr/local/bin/terraform
 sudo chmod +x /usr/local/bin/terraform
 
-sudo pacman -Sy --noconfirm docker
+### Docker
+sp docker
 sudo usermod -aG docker d33p
 sudo systemctl enable docker
 sudo systemctl start docker
+
+### gcloud
+curl https://sdk.cloud.google.com | bash
+gcloud init
+
+### kubectl
+gcloud components install kubectl
+
+### helm
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+
+### yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd .. && rm -rf yay
+
+### exa
+cargo install exa
+
+### bat
+sp -S bat
